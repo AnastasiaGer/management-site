792ebd794b34f3fde1cdccf393dd28b1d620f1b7
anisa <aagermanova@gmail.com>
2022-02-07 17:12:10 -0800

    Navbar 155

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Create from './pages/create/Create';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import Project from './pages/project/Project';
+import Navbar from './components/Navbar/Navbar';
 
 import './App.css'
 
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <div className="container">
+          <Navbar />
           <Switch>
             <Route exact path='/'>
               <Dashboard />
