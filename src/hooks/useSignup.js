8cd15a9b5b70c0f535c177f38653570819a0593b
anisa <aagermanova@gmail.com>
2022-02-08 18:20:40 -0800

    useSignup 162

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { projectAuth, projectStorage } from '../firebase/config'
+import { projectAuth, projectStorage, projectFirestore } from '../firebase/config'
 import { useAuthContext } from './useAuthContext'
 
 export const useSignup = () => {
@@ -28,6 +28,13 @@ export const useSignup = () => {
       // add display AND PHOTO_URL name to user
       await res.user.updateProfile({ displayName, photoURL: imgUrl })
 
+      // create a user document
+      await projectFirestore.collection('users').doc(res.user.uid).set({ 
+        online: true,
+        displayName,
+        photoURL: imgUrl,
+      })
+
       // dispatch login action
       dispatch({ type: 'LOGIN', payload: res.user })
 
@@ -35,8 +42,8 @@ export const useSignup = () => {
         setIsPending(false)
         setError(null)
       }
-    }
-    catch (err) {
+    } 
+    catch(err) {
       if (!isCancelled) {
         setError(err.message)
         setIsPending(false)
