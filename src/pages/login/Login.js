965df4668fd2d59440081e951025d0b9da543463
anisa <aagermanova@gmail.com>
2022-02-09 14:20:47 -0800

    Login 164-165

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,11 +1,45 @@
+import { useState } from 'react'
+
+// styles
 import './Login.css'
 
-const Login = () => {
-  return ( 
-    <div>
-      Login
-    </div>
-   );
+import { useLogin } from '../../hooks/useLogin'
+
+export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { login, isPending, error } = useLogin()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    login(email, password)
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="auth-form">
+      <h2>login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      {!isPending && <button className="btn">Log in</button>}
+      {isPending && <button className="btn" disabled>Logging in...</button>}
+      {error && <div className="error">{ error }</div>}
+    </form>
+  )
 }
- 
-export default Login;
