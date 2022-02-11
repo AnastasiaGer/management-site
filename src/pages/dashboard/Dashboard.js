3ab52a7817fd7b0d544fb13f935a0ec45b6eb8ee
anisa <aagermanova@gmail.com>
2022-02-10 18:20:26 -0800

    Dashboard 177

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,11 +1,19 @@
+import { useCollection } from '../../hooks/useCollection'
+
+// components
+import ProjectList from '../../components/ProjectList/ProjectList'
+
+// styles
 import './Dashboard.css'
 
-const Dashboard = () => {
-  return ( 
+export default function Dashboard() {
+  const { documents, error } = useCollection('projects')
+
+  return (
     <div>
-      Dashboard
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectList projects={documents} />}
     </div>
-   );
+  )
 }
- 
-export default Dashboard;
