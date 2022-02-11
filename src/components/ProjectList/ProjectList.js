c131e8a8b674558c8b6202d768134b2b0298f84c
anisa <aagermanova@gmail.com>
2022-02-10 18:25:48 -0800

    ProjectList 178

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -1,11 +1,30 @@
+import { Link } from 'react-router-dom'
+import Avatar from '../../components/Avatar/Avatar'
+
+// styles
 import './ProjectList.css'
 
 export default function ProjectList({ projects }) {
+  console.log(projects)
+
   return (
-    <div>
+    <div className="project-list">
       {projects.length === 0 && <p>No projects yet!</p>}
       {projects.map(project => (
-        <div key={project.id}>{project.name}</div> 
+        <Link to={`/projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          <div className="assigned-to">
+            <p><strong>Assigned to:</strong></p>
+            <ul>
+              {project.assignedUsersList.map(user => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
       ))}
     </div>
   )
