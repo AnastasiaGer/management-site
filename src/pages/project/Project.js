5cf35bc1ec670db0ab8ed596c29aeece63cf8c79
anisa <aagermanova@gmail.com>
2022-02-12 17:37:59 -0800

    ProjectSummary 181

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -4,6 +4,8 @@ import { useDocument } from '../../hooks/useDocument'
 // styles
 import './Project.css'
 
+import ProjectSummary from "./ProjectSummary"
+
 export default function Project() {
   const { id } = useParams()
   const { document, error } = useDocument('projects', id)
@@ -17,7 +19,7 @@ export default function Project() {
 
   return (
     <div className="project-details">
-      <h1>{document.name}</h1>
+      <ProjectSummary project={document} />
     </div>
   )
 }
