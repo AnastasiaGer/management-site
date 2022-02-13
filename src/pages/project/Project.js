37aa861182f09c4d50c2fa5dc39be0109e01ce01
anisa <aagermanova@gmail.com>
2022-02-12 17:32:34 -0800

    Project 180

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,11 +1,23 @@
+import { useParams } from "react-router-dom"
+import { useDocument } from '../../hooks/useDocument'
+
+// styles
 import './Project.css'
 
-const Project = () => {
-  return ( 
-    <div>
-      Project
+export default function Project() {
+  const { id } = useParams()
+  const { document, error } = useDocument('projects', id)
+
+  if (error) {
+    return <div className="error">{error}</div>
+  }
+  if (!document) {
+    return <div className="loading">Loading...</div>
+  }
+
+  return (
+    <div className="project-details">
+      <h1>{document.name}</h1>
     </div>
-   );
+  )
 }
- 
-export default Project;
